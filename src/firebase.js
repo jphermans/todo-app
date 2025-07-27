2a0e79b672f30253c7f81ad3493b42df0de7ba2c
jphermans <munthouder.merrie-0w@icloud.com>
2025-07-27 19:22:24 +0200

    feat: update Firebase configuration with project-specific details

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getDatabase, ref, set, get, onValue, off } from 'firebase/database';
 
 // Your Firebase configuration - replace with your actual config from Firebase Console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCQhA8Squ_Fq_iyiv3EEUQX_3uVkWDEsHw",
+  authDomain: "todo-944c5.firebaseapp.com",
+  databaseURL: "https://todo-944c5-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todo-944c5",
+  storageBucket: "todo-944c5.firebasestorage.app",
+  messagingSenderId: "233006683224",
+  appId: "1:233006683224:web:c80aaa59dbe77bab5c262d",
+  measurementId: "G-7C4568C9HE"
 };
 
 // Initialize Firebase
